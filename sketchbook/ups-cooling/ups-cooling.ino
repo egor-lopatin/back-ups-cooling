428508660ae74d0dc435373d1d0b7bfddfb42986
root <root@zm.justonpi.com>
2019-10-24 14:10:38 +0300

    loop optimization

--- sketchbook/ups-cooling/ups-cooling.ino
+++ sketchbook/ups-cooling/ups-cooling.ino
@@ -1,15 +1,16 @@
-#include <OneWire.h> // Инициализация библиотеки шины OneWire.
-#include <DallasTemperature.h> // Инициализация библиотеки термодатчиков.
-
-#define ONE_WIRE_BUS 10// Подключение цифрового вывода датчика к 10-му пину Ардуино.
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 10
 
 int Relay = 8;
-float t_current = 0;
 int t_limit = 30;
 int hysterisis = 1;
+float t_current = 0;
 
-OneWire oneWire(ONE_WIRE_BUS); // Запуск интерфейса OneWire для подключения OneWire устройств.
-DallasTemperature sensors(&oneWire); // Указание, что устройством oneWire является термодатчик от  Dallas Temperature.
+int fan_enabled = 0;
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
 void readsensor() {
   sensors.requestTemperatures();
@@ -17,30 +18,32 @@ void readsensor() {
 }
 
 void setup(void){
-  Serial.begin(9600); // Запуск СОМ порта.
-  sensors.begin(); // Запуск сенсора.
+  Serial.begin(9600);
+
+  sensors.begin();
   pinMode(Relay, OUTPUT);
-  digitalWrite(Relay, HIGH);  // реле выключено
-  readsensor(); // read sensor function
+  digitalWrite(Relay, HIGH);
+
+  readsensor();
 }
 
 void loop(void){ 
   readsensor();
   Serial.println(t_current);
 
-  if (t_current < t_limit){
-      digitalWrite(Relay, HIGH);  // реле выключено
-  }
-  else {
-      digitalWrite(Relay, LOW);   // реле включено
+  if (t_current > t_limit){
+    digitalWrite(Relay, LOW);
+    fan_enabled = 1;
 
-      do {
-        readsensor();
-        Serial.println(t_current);
-        delay(1000);
-      } while (t_current > (t_limit - hysterisis));
+    do {
+       readsensor();
+       Serial.println(t_current);
+       delay(1000);
+       } while (t_current > (t_limit - hysterisis));
 
-      digitalWrite(Relay, HIGH);
+    digitalWrite(Relay, HIGH);
+    fan_enabled = 0;
   }
+
   delay(1000);
 }
