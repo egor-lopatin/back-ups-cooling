384998d20f24430a87e0e58a3e093b8c59a665c8
root <root@zm.justonpi.com>
2019-12-20 12:01:02 +0200

    The second sensor added | Misc changes

--- sketchbook/ups-cooling/ups-cooling.ino
+++ sketchbook/ups-cooling/ups-cooling.ino
@@ -5,34 +5,36 @@
 #include <DallasTemperature.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <Wire.h>
 
 #define LCDVCC 2
 #define MOTOR 3
 #define RELAYGND 4
 #define RELAY 6
 #define RELAYVCC 5
-#define DSIN 12
-#define DSIN_GND 13
-#define DSOUT 9
-#define DSOUT_GND 10
-#define DSOUT_VCC 11
-
+#define DSOUT 12
+#define DSOUT_GND 13
+#define DSIN 9
+#define DSIN_GND 10
+#define DSIN_VCC 11
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-
 ServoTimer2 motor;
-
 OneWire oneWireIn(DSIN);
-  DallasTemperature sensorin(&oneWireIn);
+DallasTemperature sensorin(&oneWireIn);
 OneWire oneWireOut(DSOUT);
-  DallasTemperature sensorout(&oneWireOut);
+DallasTemperature sensorout(&oneWireOut);
 
 // sensors vars
-int temp_limit = 35;
-int temp_hyst = 2;
-float temp_current = 0;
-float temp_error = -127.0;
-bool high_temp = false;
+float t_error = -127.0;
+
+int t_in_limit = 35;
+int t_in_hyst = 2;
+float t_in_current = 0;
+
+int t_out_limit = 28;
+int t_out_hyst = 2;
+float t_out_current = 0;
 
 // lcd lines length
 char line0[20]; 
@@ -51,18 +53,28 @@ void readDS() {
   sensorin.requestTemperatures();
   sensorout.requestTemperatures();
 
-  if (sensorout.getTempCByIndex(0) != temp_error) {
-    temp_current = sensorout.getTempCByIndex(0);
+  if (sensorin.getTempCByIndex(0) != t_error) {
+     t_in_current = sensorin.getTempCByIndex(0);
   } else {
-    temp_current = 0;
+     t_in_current = 0;
+  }
+
+  if (sensorout.getTempCByIndex(0) != t_error) {
+    t_out_current = sensorout.getTempCByIndex(0);
+  } else {
+    t_out_current = 0;
   }
 }
 
 void updateDisplay() {
-   char floatemp_str[7];
-   dtostrf(temp_current,4,1,floatemp_str);
+   char float_in_str[7];
+   dtostrf(t_in_current,4,1,float_in_str);
 
-   sprintf(line0, "Current Temp: %-5s", floatemp_str);
+   char float_out_str[7];
+   dtostrf(t_out_current,4,1,float_out_str);
+
+   sprintf(line0, "UPS Temp: %-5s", float_in_str);
+   sprintf(line1, "Room Temp: %-5s", float_out_str);
 
    lcd.setCursor(0,0);
    lcd.print(line0);
@@ -106,36 +118,44 @@ void maxFan() {
   delay(20);
 }
 
+void printSensors() {
+  char t_in[50];
+  char t_out[50];
+  sprintf(t_in, "%s ", t_in_current);
+  sprintf(t_out, "%s", t_out_current);
+
+//  Serial.print(t_in);
+//  Serial.println(t_out);
+  Serial.println(t_in_current);
+//  Serial.println(t_out_current);
+}
+
 void setup(void) {
   Serial.begin(9600);
 
-  // VCC and GND pinouts
   pinMode(RELAYVCC, OUTPUT);
     digitalWrite(RELAYVCC, 1);
   pinMode(RELAYGND, OUTPUT);
     digitalWrite(RELAYGND, 0);
   pinMode(LCDVCC, OUTPUT);
     digitalWrite(LCDVCC, 1);
-  pinMode(DSIN_GND, OUTPUT);
-    digitalWrite(DSIN_GND, 0);
-  pinMode(DSOUT_VCC, OUTPUT);
-    digitalWrite(DSOUT_VCC, 1);
   pinMode(DSOUT_GND, OUTPUT);
-    digitalWrite(DSOUT_GND, 0);  
+    digitalWrite(DSOUT_GND, 0);
+  pinMode(DSIN_VCC, OUTPUT);
+    digitalWrite(DSIN_VCC, 1);
+  pinMode(DSIN_GND, OUTPUT);
+    digitalWrite(DSIN_GND, 0);  
 
-  // Relay initial
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, 1);
 
-  // Sensors initial
   sensorin.begin();
   sensorout.begin();
 
-  // LCD initial
   lcd.begin();
 //  lcd.backlight();
+  lcd.noBacklight();
 
-  // ESC initial
   initMotor();
 }
 
@@ -143,20 +163,20 @@ void setup(void) {
 
 void loop(void) {
   readDS();
-    Serial.println(temp_current);
+  printSensors();
 
   updateDisplay();
 
-  if (temp_current > temp_limit) {
+  if (t_in_current > t_in_limit) {
     digitalWrite(RELAY, 0);
     do {
       readDS();
-        Serial.println(temp_current);
+      printSensors();
 
       updateDisplay();
       delay(1000);
       
-    } while (temp_current > (temp_limit - temp_hyst));
+    } while (t_in_current > (t_in_limit - t_in_hyst));
     digitalWrite(RELAY, 1);
   }
   delay(1000);
